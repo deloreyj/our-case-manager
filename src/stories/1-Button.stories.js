98c1a0be7d0b55b1902f0bb115ffa4ac9bd5dc27
grigg <grigg@adobe.com>
2020-08-03 23:13:57 -0600

    Add spectrum button stories

--- src/stories/1-Button.stories.js
+++ src/stories/1-Button.stories.js
@@ -1,18 +1,39 @@
-import React from 'react';
-import { action } from '@storybook/addon-actions';
-import { Button } from '@storybook/react/demo';
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+import { Provider, defaultTheme, Button } from "@adobe/react-spectrum";
 
 export default {
-  title: 'Button',
-  component: Button,
+  title: "Spectrum Buttons",
+  decorators: [
+    (storyFn) => (
+      <Provider theme={defaultTheme} colorScheme="light">
+        {storyFn()}
+      </Provider>
+    ),
+  ],
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button</Button>;
+export const CTA = () => (
+  <Button variant="cta" onClick={action("clicked")}>
+    I Call Thee To Action!
+  </Button>
+);
 
-export const Emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
+export const Negative = () => (
+  <Button variant="negative" onClick={action("clicked")}>
+    Be Careful!
+  </Button>
+);
+
+export const Primary = () => (
+  <Button variant="primary" onClick={action("clicked")}>
+    Primary
+  </Button>
+);
+
+export const Secondary = () => (
+  <Button variant="secondary" onClick={action("clicked")}>
+    Secondary
   </Button>
 );
